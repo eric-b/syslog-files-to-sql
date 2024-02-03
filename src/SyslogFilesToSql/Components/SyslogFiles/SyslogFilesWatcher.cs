b20d10a28a6f2cfd555ef43b830093d73084aaf1
Eric Boumendil <eric.bml@fastmail.fr>
2024-02-03 11:42:21 +0100

    Minor

--- src/SyslogFilesToSql/Components/SyslogFiles/SyslogFilesWatcher.cs
+++ src/SyslogFilesToSql/Components/SyslogFiles/SyslogFilesWatcher.cs
@@ -6,7 +6,6 @@ using Microsoft.Extensions.Primitives;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Threading.Tasks;
 
 namespace SyslogFilesToSql.Components.SyslogFiles
 {
